25ace6453de37242539b651b5630bd2e2f095f42
GrappleStiltskin <94195837+GrappleStiltskin@users.noreply.github.com>
2024-06-17 15:22:04 -0500

    Update oscp-class-install1.sh

--- oscp-class-install1.sh
+++ oscp-class-install1.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Install enum4linux-ng
+apt-get install enum4linux-ng;
+
 # Install Autorecon
 apt install python3;
 apt install python3-pip;
 apt install python3-venv;
+apt install seclists;
+apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf;
 python3 -m pip install --user pipx;
 python3 -m pipx ensurepath;
+
+# Close and re-open terminal, then run:
+# env "PATH=$PATH" autorecon;
+# pipx install git+https://github.com/Tib3rius/AutoRecon.git;
