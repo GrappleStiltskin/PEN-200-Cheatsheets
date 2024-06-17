8e622be49f524b6473466517c8876cdda9b55857
GrappleStiltskin <94195837+GrappleStiltskin@users.noreply.github.com>
2024-06-17 16:00:39 -0500

    Update oscp-class-install.sh

--- oscp-class-install.sh
+++ oscp-class-install.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Update and upgrade
-apt-get update --fix-missing;
-apt-get upgrade --fix-missing;
+apt-get update --fix-missing -y;
+apt-get upgrade --fix-missing -y;
 
 # Install enum4linux-ng
-apt-get install enum4linux-ng;
+apt-get install enum4linux-ng -y;
 
 # Install winpeas
 wget https://github.com/peass-ng/PEASS-ng/releases/download/20240616-43d0a061/winPEASx64.exe;
 
 # Install Autorecon
-apt install python3;
-apt install python3-pip;
-apt install python3-venv;
-apt install seclists;
-apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf;
-python3 -m pip install --user pipx;
-python3 -m pipx ensurepath;
+apt install python3 -y;
+apt install python3-pip -y;
+apt install python3-venv -y;
+apt install seclists -y;
+apt install seclists curl dnsrecon enum4linux feroxbuster gobuster impacket-scripts nbtscan nikto nmap onesixtyone oscanner redis-tools smbclient smbmap snmp sslscan sipvicious tnscmd10g whatweb wkhtmltopdf -y;
+python3 -m pip install --user pipx -y;
+python3 -m pipx ensurepath -y;
 
 # Close and re-open terminal, then run:
 # pipx install git+https://github.com/Tib3rius/AutoRecon.git;
